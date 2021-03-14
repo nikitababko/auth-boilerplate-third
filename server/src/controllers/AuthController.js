2a7ee010dfd617ddc7ac121edf462e4506b92d26
Nikita Babko <niki-babko@yandex.ru>
2021-03-14 12:02:13 +0700

    Added activation.

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -6,6 +6,11 @@ const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const sgMail = require('@sendgrid/mail');
 
+// Config .env to ./config/config.env
+require('dotenv').config({
+  path: 'src/config/config.env',
+});
+
 sgMail.setApiKey(process.env.MAIL_KEY);
 
 // Custom error handler to get useful error from database errors
@@ -13,13 +18,14 @@ const { errorHandler } = require('../helpers/dbErrorHandling');
 // User model
 const User = require('../models/User');
 
+// Register
 exports.register = (req, res) => {
   const { name, email, password } = req.body;
   const errors = validationResult(req);
 
   // validation to req.body we will create custom validation in seconds
   if (!errors.isEmpty()) {
-    const firstError = errors.array().map((errors) => errors.msg)[0];
+    const firstError = errors.array().map((error) => error.msg)[0];
     return res.status(422).json({
       errors: firstError,
     });
@@ -51,7 +57,7 @@ exports.register = (req, res) => {
     // Email data sending
     const emailData = {
       from: process.env.EMAIL_FROM,
-      to: to,
+      to: email,
       subject: 'Account activation link',
       html: `
           <h1>Please click to link to activate</h1>
@@ -61,5 +67,68 @@ exports.register = (req, res) => {
           <p>${process.env.CLIENT_URL}<p/>
       `,
     };
+
+    sgMail
+      .send(emailData)
+      .then((sent) => {
+        return res.json({
+          message: `Email has been sent to ${email}`,
+        });
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          success: false,
+          errors: errorHandler(err),
+        });
+      });
+  }
+};
+
+// Activation and save to database
+exports.activation = (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    // Verify the token is valid or not or expired
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+      if (err) {
+        console.log('Activation error');
+        return res.status(401).json({
+          errors: 'Expired link. Signup again',
+        });
+      } else {
+        /**
+         * If valid save to database
+         * Get name, email, password from token
+         */
+        const { name, email, password } = jwt.decode(token);
+
+        console.log(email);
+        const user = new User({
+          name,
+          email,
+          password,
+        });
+
+        user.save((err, user) => {
+          if (err) {
+            console.log('Save error', errorHandler(err));
+            return res.status(401).json({
+              errors: errorHandler(err),
+            });
+          } else {
+            return res.json({
+              success: true,
+              message: 'Signup success',
+              user,
+            });
+          }
+        });
+      }
+    });
+  } else {
+    return res.json({
+      message: 'error happening please try again',
+    });
   }
 };
