566a4dc44e056e13ff6ed7659f815076dd96139d
Nikita Babko <niki-babko@yandex.ru>
2021-03-14 19:08:48 +0700

    Added route to reset and forgot password.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import App from './App';
-import { Activate, Register } from './screens';
+import { Login, Activate, Register, ForgotPassword, ResetPassword } from './screens';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,11 +11,22 @@ ReactDOM.render(
   <Router>
     <Switch>
       <Route exact path="/" render={(props) => <App {...props} />} />
+      <Route exact path="/login" render={(props) => <Login {...props} />} />
       <Route exact path="/register" render={(props) => <Register {...props} />} />
       <Route
         exact
         path="/users/activate/:token"
         render={(props) => <Activate {...props} />}
+      />
+      <Route
+        exact
+        path="/users/password/reset/:token"
+        render={(props) => <ResetPassword {...props} />}
+      />
+      <Route
+        exact
+        path="/users/password/forgot"
+        render={(props) => <ForgotPassword {...props} />}
       />
     </Switch>
   </Router>,
