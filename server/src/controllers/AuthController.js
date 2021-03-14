1d816e4d5e3021a64f6cdd6f6a62a7ec0fc145a9
Nikita Babko <niki-babko@yandex.ru>
2021-03-14 18:37:18 +0700

    Added login and forgot password.

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -132,3 +132,131 @@ exports.activation = (req, res) => {
     });
   }
 };
+
+// Login
+exports.login = (req, res) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map((error) => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError,
+    });
+  } else {
+    // check if user exist
+    User.findOne({
+      email,
+    }).exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          errors: 'User with that email does not exist. Please signup',
+        });
+      }
+      // authenticate
+      if (!user.authenticate(password)) {
+        return res.status(400).json({
+          errors: 'Email and password do not match',
+        });
+      }
+      // generate a token and send to client
+      const token = jwt.sign(
+        {
+          _id: user._id,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '7d',
+        }
+      );
+      const { _id, name, email, role } = user;
+
+      return res.json({
+        token,
+        user: {
+          _id,
+          name,
+          email,
+          role,
+        },
+      });
+    });
+  }
+};
+
+// Forgot password
+exports.forgotPassword = (req, res) => {
+  const { email } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map((error) => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError,
+    });
+  } else {
+    User.findOne(
+      {
+        email,
+      },
+      (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({
+            error: 'User with that email does not exist',
+          });
+        }
+
+        const token = jwt.sign(
+          {
+            _id: user._id,
+          },
+          process.env.JWT_RESET_PASSWORD,
+          {
+            expiresIn: '10m',
+          }
+        );
+
+        const emailData = {
+          from: process.env.EMAIL_FROM,
+          to: email,
+          subject: `Password reset link`,
+          html: `
+                    <h1>Please use the following link to reset your password</h1>
+                    <p>${process.env.CLIENT_URL}/users/password/reset/${token}</p>
+                    <hr />
+                    <p>This email may contain sensetive information</p>
+                    <p>${process.env.CLIENT_URL}</p>
+                `,
+        };
+
+        return user.updateOne(
+          {
+            resetPasswordLink: token,
+          },
+          (err, success) => {
+            if (err) {
+              console.log('RESET PASSWORD LINK ERROR', err);
+              return res.status(400).json({
+                error: 'Database connection error on user password forgot request',
+              });
+            } else {
+              sgMail
+                .send(emailData)
+                .then((sent) => {
+                  // console.log('SIGNUP EMAIL SENT', sent)
+                  return res.json({
+                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+                  });
+                })
+                .catch((err) => {
+                  // console.log('SIGNUP EMAIL SENT ERROR', err)
+                  return res.json({
+                    message: err.message,
+                  });
+                });
+            }
+          }
+        );
+      }
+    );
+  }
+};
