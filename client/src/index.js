acc2d27aba8238315fc600b9ccfbf13c727b76a8
Nikita Babko <niki-babko@yandex.ru>
2021-03-14 13:41:23 +0700

    Added route path to activate.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import App from './App';
-import { Register } from './screens';
+import { Activate, Register } from './screens';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,6 +12,11 @@ ReactDOM.render(
     <Switch>
       <Route exact path="/" render={(props) => <App {...props} />} />
       <Route exact path="/register" render={(props) => <Register {...props} />} />
+      <Route
+        exact
+        path="/users/activate/:token"
+        render={(props) => <Activate {...props} />}
+      />
     </Switch>
   </Router>,
   document.getElementById('root')
