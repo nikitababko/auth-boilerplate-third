6d7f1f6de636ae60ffe2926364fd87abb0798cdc
Nikita Babko <niki-babko@yandex.ru>
2021-03-14 12:03:44 +0700

    Imported helpers and added login route, activation route.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -1,8 +1,18 @@
 // Load controllers
 const AuthController = require('../controllers/AuthController');
 
+// Import validations helpers
+const {
+  validSign,
+  validLogin,
+  forgotPasswordValidator,
+  resetPasswordValidator,
+} = require('../helpers/valid');
+
 const createRoutes = (app) => {
-  app.post('/api/register', AuthController.register);
+  app.post('/api/register', validSign, AuthController.register);
+  app.post('/api/login', validSign, AuthController.login);
+  app.post('/api/activation', AuthController.activation);
 };
 
 module.exports = createRoutes;
