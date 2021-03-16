246891cd2a6fe0dd47e70200ed1f0b33e88abda0
Nikita Babko <niki-babko@yandex.ru>
2021-03-16 22:11:39 +0700

    Added google login controller.

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -319,3 +319,59 @@ exports.resetPassword = (req, res) => {
     }
   }
 };
+
+// Google login
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT);
+
+exports.googleLogin = (req, res) => {
+  const { idToken } = req.body;
+
+  client
+    .verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT })
+    .then((response) => {
+      // console.log('GOOGLE LOGIN RESPONSE',response)
+      const { email_verified, name, email } = response.payload;
+      if (email_verified) {
+        User.findOne({ email }).exec((err, user) => {
+          if (user) {
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+              expiresIn: '7d',
+            });
+
+            const { _id, email, name, role } = user;
+            // Send response to client side(react) token and user info
+            return res.json({
+              token,
+              user: { _id, email, name, role },
+            });
+          } else {
+            // If user not exists we will save in database and generate password for it
+            let password = email + process.env.JWT_SECRET;
+            user = new User({ name, email, password });
+            user.save((err, data) => {
+              if (err) {
+                console.log('ERROR GOOGLE LOGIN ON USER SAVE', err);
+                return res.status(400).json({
+                  error: 'User signup failed with google',
+                });
+              }
+              // If no error generate token
+              const token = jwt.sign({ _id: data._id }, process.env.JWT_SECRET, {
+                expiresIn: '7d',
+              });
+              const { _id, email, name, role } = data;
+              return res.json({
+                token,
+                user: { _id, email, name, role },
+              });
+            });
+          }
+        });
+      } else {
+        // If error
+        return res.status(400).json({
+          error: 'Google login failed. Try again',
+        });
+      }
+    });
+};
