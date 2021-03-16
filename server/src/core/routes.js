57a5cde6ef19802ca5ab81c1dfce7db8e7fc427b
Nikita Babko <niki-babko@yandex.ru>
2021-03-16 22:12:10 +0700

    Added google-login route.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -15,6 +15,8 @@ const createRoutes = (app) => {
   app.post('/api/activation', AuthController.activation);
   app.put('/api/password/forgot', forgotPasswordValidator, AuthController.forgotPassword);
   app.put('/api/password/reset', resetPasswordValidator, AuthController.resetPassword);
+
+  app.post('/api/googlelogin', AuthController.googleLogin);
 };
 
 module.exports = createRoutes;
