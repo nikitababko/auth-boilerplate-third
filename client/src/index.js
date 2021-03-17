cecb906c3a5f0ef75a8cace50312e3050f574d7d
Nikita Babko <niki-babko@yandex.ru>
2021-03-17 11:29:38 +0700

    Added route to private and admin.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import App from './App';
-import { Login, Activate, Register, ForgotPassword, ResetPassword } from './screens';
+import {
+  Login,
+  Activate,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Private,
+  Admin,
+} from './screens';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -28,6 +36,9 @@ ReactDOM.render(
         path="/users/password/forgot"
         render={(props) => <ForgotPassword {...props} />}
       />
+      <PrivateRoute path="/private" exact component={Private} />
+      <AdminRoute path="/admin" exact component={Admin} />
+      <Redirect to="/" />
     </Switch>
   </Router>,
   document.getElementById('root')
