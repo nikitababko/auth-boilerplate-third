313c613aaff3baac6e90abfa5ed55314b5af690c
Nikita Babko <niki-babko@yandex.ru>
2021-03-17 15:05:49 +0700

    Added admin middleware.

---
+++ server/src/middlewares/admin.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+exports.adminMiddleware = (req, res, next) => {
+  User.findById({
+    _id: req.user._id,
+  }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User not found',
+      });
+    }
+
+    if (user.role !== 'admin') {
+      return res.status(400).json({
+        error: 'Admin resource. Access denied.',
+      });
+    }
+
+    req.profile = user;
+    next();
+  });
+};
