8d39dcfea9b9c21b757e1f6b3a5b13519a7aea8d
Nikita Babko <niki-babko@yandex.ru>
2021-03-17 11:09:29 +0700

    Exported Admin page.

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -3,3 +3,4 @@ export { default as Activate } from './Activate';
 export { default as Login } from './Login';
 export { default as ForgotPassword } from './ForgotPassword';
 export { default as ResetPassword } from './ResetPassword';
+export { default as Admin } from './Admin';
